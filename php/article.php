5fd5debfba757e200334d6cbffa9714a7ce8efde
Yavor Ivanov <davko996@gmail.com>
2014-08-21 16:43:05 +0300

    Layout Changes. Simple php changes

--- php/article.php
+++ php/article.php
@@ -9,15 +9,34 @@ $article = <<<EOD
                     <img src="images/off.png" alt="off" class="topicImage"/>
                 </td>
                 <td class="topicDescription">
-                    
+                    <table class="topicTable">
+                        <tbody>
+                            <tr><td>General Discussion</td></tr>
+                            <tr><td>Feel free to talk about anything and everything in this board.</td></tr>
+                        </tbody>
+                    </table>
                 </td>
 
                 <td class="topicStats">
+                     <table class="statsTable">
+                        <tbody>
+EOD;
 
+$myTrOne = "<tr><td>" . rand(0, 35) . " posts</td></tr>";
+$myTrTwo = "<tr><td>" . rand(0,35) . " topics</td></tr>";
+
+$articleEndPart =  <<<EOD
+                        </tbody>
+                    </table>
                 </td>
 
                 <td class="topicLastPost">
-
+                    <table class="lastPostTable">
+                        <tbody>
+                            <tr><td><b>Pesho</b> in <span>Lorem Ipsum</span></td></tr>
+                            <tr><td>October 15, 2010, 07:34:09 AM</td></tr>
+                        </tbody>
+                    </table>
                 </td>
             </tr>
         </tbody>
@@ -25,5 +44,5 @@ $article = <<<EOD
 </article>
 EOD;
 
-echo $article;
+echo $article . $myTrOne . $myTrTwo . $articleEndPart;
 
