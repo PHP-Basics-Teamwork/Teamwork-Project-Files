5d9c66e6574d7456422e73eebe576d673d0ef436
MartinBorisov94 <martinborisov94@gmail.com>
2014-08-21 18:26:15 +0300

    profile update

---
+++ profile.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="styles/styles.css"/>
+    <link rel="stylesheet" href="styles/profileStyles.css"/>
+    <meta charset="utf8">
+</head>
+<body>
+<header>
+    <img src="images/logo.png" alt="" class="logo"/>
+
+</header>
+<main>
+    <section>
+        <header>
+            <h1>USER INFORMATION</h1>
+        </header>
+               <ul id="basicinfo">
+                   <li><h4>User Name</h4></li>
+                   <li><h5 class="postition">Administrator</h5></li>
+                   <li><img src="resources/userPic.jpg" alt="userPic"></li>
+                   <li><div id="userstatus">Offline</div></li>
+                   <li><a href="#" >Show Posts</a></li>
+                   <li><a href="#" >Show Stats</a></li>
+               </ul>
+        <div id="aboutUser">
+            <ul>
+                <li><span class="profile-label">Posts:</span> 15</li>
+                <li><span class="profile-label">Personal Text: </span> Some txt</li>
+                <li><span class="profile-label">Gender: </span> Male</li>
+                <li><span class="profile-label">Age:</span> 101</li>
+                <li><span class="profile-label">Date Registered: </span> September 25, 2008, 09:00:42 PM</li>
+                <li><span class="profile-label">Local Time:</span> August 21, 2014, 06:06:41 AM</li>
+                <li><span class="profile-label">Last Active: </span> July 07, 2014, 03:16:56 AM</li>
+            </ul>
+            <h6>Signature:</h6>
+            <p id="signature">
+                some txt some txtsome txtsome txtsome txtsome txtsome txtsome txtsome txtsome txtsome txt
+                some txt some txtsome txtsome txtsome txtsome txtsome txtsome txtsome txtsome txtsome txt
+                some txt some txtsome txtsome txtsome txtsome txtsome txtsome txtsome txtsome txtsome txt
+                some txt some txtsome txtsome txtsome txtsome txtsome txtsome txtsome txtsome txtsome txt
+                some txt some txtsome txtsome txtsome txtsome txtsome txtsome txtsome txtsome txtsome txt dsa das
+                signa t uresi gnature signa tur esigna t uresi gnature signa tur e signa t uresi gnature signa tur e
+                signa t uresi gnature signa tur e signa t uresi gnature signa tur e signa t uresi gnature signa tur e
+            </p>
+        </div>
+    </section>
+
+
+</main>
+
+<footer></footer>
+</body>
+</html>
