e1082bd6a2a2e72f2f79f424b8fcce700eb3f138
dessso <desislav_d@abv.bg>
2014-08-23 23:54:25 +0300

    update
    update

--- views/aboutUs.php
+++ views/aboutUs.php
@@ -9,19 +9,19 @@
 <br/>
 <a href="">Georgi Georgiev</a>
 <img src="../images/GeorgiGeorgievPic.jpg" alt="pic Georgi" height="62" width="62">
-
+<br/>
 <a href="">Yavor Ivanov</a>
 <img src="../images/YavorIvanovPic.jpg" alt="pic Yavor" height="62" width="62">
-
+<br/>
 <a href="">Martin Borisov</a>
 <img src="../images/MartinBorisovPic.jpg" alt="pic Martin" height="62" width="62">
-
+<br/>
 <a href="">Luboslav Pirsafov</a>
 <img src="../images/LuboslavPirsafovPic.jpg" alt="pic Luboslav" height="62" width="62">
-
+<br/>
 <a href="">Desislav Delchev</a>
 <img src="../images/DesislavDelchevPic.jpg" alt="pic Desislav" height="62" width="62">
-
+<br/>
 <a href="">Gabriel Syarov</a>
 <img src="../images/GabrielSyarovPic.jpg" alt="pic Gabriel" height="62" width="62">
 
