b4b7b49dd042208ce48f587a0541f109ac923036
dessso <desislav_d@abv.bg>
2014-08-24 00:57:54 +0300

    update about us - page
    update

--- views/aboutUs.php
+++ views/aboutUs.php
@@ -2,28 +2,29 @@
 <html>
 <head>
     <title>About Us</title>
+    <link rel="stylesheet" href="../styles/styles.css" />
 </head>
 <body>
-
+<br/>
 <div> Who we are? We are the Lace Fern Team from the Software University - we are 6 great software developers creating this amazing forum!</div>
 <br/>
-<a href="">Georgi Georgiev</a>
+<div class="aboutUs1"><a href="">Georgi Georgiev </a>
 <img src="../images/GeorgiGeorgievPic.jpg" alt="pic Georgi" height="62" width="62">
-<br/>
-<a href="">Yavor Ivanov</a>
+</div>
+<div class="aboutUs2"><a href="">Yavor Ivanov </a>
 <img src="../images/YavorIvanovPic.jpg" alt="pic Yavor" height="62" width="62">
-<br/>
-<a href="">Martin Borisov</a>
+</div>
+<div class="aboutUs3"><a href="">Martin Borisov </a>
 <img src="../images/MartinBorisovPic.jpg" alt="pic Martin" height="62" width="62">
-<br/>
-<a href="">Luboslav Pirsafov</a>
+</div>
+<div class="aboutUs4"><a href="">Luboslav Pirsafov </a>
 <img src="../images/LuboslavPirsafovPic.jpg" alt="pic Luboslav" height="62" width="62">
-<br/>
-<a href="">Desislav Delchev</a>
+</div>
+<div class="aboutUs5"><a href="">Desislav Delchev </a>
 <img src="../images/DesislavDelchevPic.jpg" alt="pic Desislav" height="62" width="62">
-<br/>
-<a href="">Gabriel Syarov</a>
+</div>
+<div class="aboutUs6"><a href="">Gabriel Syarov </a>
 <img src="../images/GabrielSyarovPic.jpg" alt="pic Gabriel" height="62" width="62">
-
+</div>
 </body>
 </html>
