95bc627abde40ee87e63673e1d62805b4e4612bc
Georgi <gardax95@gmail.com>
2014-08-25 22:39:14 +0300

    added addPost.php page

---
+++ views/addPost.php
@@ -0,0 +1,21 @@
+<div id="addPostWrapper">
+    <form action="" method="POST">
+        <ul>
+            <li>
+                <label>Заглавие:</label>
+                <input type="text" name="title" />
+            </li>
+            <li>
+                <label>Накратко:</label>
+                <input type="text" name="summary" />
+            </li>
+            <li>
+                <label>Текст:</label>
+                <textarea name="text"></textarea>
+            </li>
+            <li>
+                <input type="submit" name="addPost" value="Добави пост"/>
+            </li>
+        </ul>
+    </form>
+</div>
