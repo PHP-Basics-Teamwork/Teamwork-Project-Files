2b2380fa7d45052cb59dde7b70ef483927f33b79
MartinBorisov94 <martinborisov94@gmail.com>
2014-08-26 22:50:14 +0300

    search.php style fix

--- views/search.php
+++ views/search.php
@@ -1,6 +1,6 @@
 <div id="search">
     <form action="index.php?page=search" method="POST">
         <input type="text" name="search" id="search" placeholder="Search...">
-        <input type="submit" name="searchButton" value="Search" />
+        <input type="submit" name="searchButton" value="Search"  id="searchButon"/>
     </form>
 </div>
